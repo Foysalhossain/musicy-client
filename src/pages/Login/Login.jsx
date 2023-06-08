c98ead97aeee66914c21346e03ca47c54cbce424
Foysal Hossain <foysalhossain772@gmail.com>
2023-06-08 18:32:38 +0600

    login page added

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,12 +1,21 @@
+import { useContext } from 'react';
 import logImg from '../../../src/assets/login/1.png'
+import { AuthContext } from '../../providers/AuthProvider';
 
 const Login = () => {
+    const { signIn } = useContext(AuthContext)
+
     const handleLogin = event => {
         event.preventDefault();
         const form = event.target;
         const email = form.email.value;
         const password = form.password.value;
         console.log(email, password);
+        signIn(email, password)
+            .then(result => {
+                const user = result.user;
+                console.log(user);
+            })
     }
 
     return (
