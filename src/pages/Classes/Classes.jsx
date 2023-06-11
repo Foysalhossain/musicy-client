a8c362810ce1b3c649ab8ce69db4646dda433dc8
Foysal Hossain <foysalhossain772@gmail.com>
2023-06-12 01:30:47 +0600

    classes page added

--- src/pages/Classes/Classes.jsx
+++ src/pages/Classes/Classes.jsx
@@ -1,9 +1,35 @@
+import { useEffect, useState } from "react";
 
 
 const Classes = () => {
+    const [datas, setDatas] = useState()
+    useEffect(() => {
+        fetch('http://localhost:5000/classes')
+            .then(res => res.json())
+            .then(data => {
+                setDatas(data);
+            })
+    }, [])
     return (
-        <div>
-
+        <div className="grid grid-cols-3 gap-6 pt-36 pb-36">
+            {
+                datas?.map(data => {
+                    return (
+                        <div key={data._id} className="card w-96 bg-base-100 shadow-xl">
+                            <figure className="px-10 pt-10">
+                                <img src={data.image} alt="" className="rounded-xl" />
+                            </figure>
+                            <div className="card-body ">
+                                <h2 className="card-title">Name: {data.name}</h2>
+                                <p>Instructor Name: {data.instructor}</p>
+                                <p>Available seats: {data.available_seats}</p>
+                                <p>Price: {data.price}</p>
+                                <button className="btn btn-active btn-primary">Select</button>
+                            </div>
+                        </div>
+                    )
+                })
+            }
         </div>
     );
 };
